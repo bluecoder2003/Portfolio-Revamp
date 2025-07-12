5f664fc0d197d2baf910e79f2a460d96f8501053
bishakhne0gi <bneogi102002@gmail.com>
2025-07-13 00:18:54 +0530

    feat: :sparkles: added the animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,26 @@ import ProjectSection from "@/components/sections/ProjectSection";
 import FooterSection from "@/components/sections/FooterSection";
 import ConnectSection from "@/components/sections/ConnectSection";
 import PersonSection from "@/components/sections/PersonSections";
+import AnimatedSection from "@/components/ui/animated-section";
 
 export default function Home() {
   return (
     <div className="h-fit flex items-center justify-center flex-col p-[80px] gap-[10px] bg-[#E6E6E6]">
-      <HeroSection />
-      <ProjectSection />
-      <PersonSection />
-      <ConnectSection />
-      <FooterSection />
+      <AnimatedSection delay={0.1}>
+        <HeroSection />
+      </AnimatedSection>
+      <AnimatedSection delay={0.2}>
+        <ProjectSection />
+      </AnimatedSection>
+      <AnimatedSection delay={0.3}>
+        <PersonSection />
+      </AnimatedSection>
+      <AnimatedSection delay={0.4}>
+        <ConnectSection />
+      </AnimatedSection>
+      <AnimatedSection delay={0.5}>
+        <FooterSection />
+      </AnimatedSection>
     </div>
   );
 }
