79531b481b5022ccae3f94a80fba5d7f3ea49624
bluecoder2003 <130232862+bluecoder2003@users.noreply.github.com>
2025-08-10 10:24:59 +0530

    feat: :zap: added proper title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,21 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Crafting Stories",
-  description: "Neelakshi Das's Portfolio",
+  title: "Pixel Stories",
+  description: "Neelakshi's Portfolio",
   keywords: "portfolio, designer, developer, UI/UX, frontend",
   authors: [{ name: "Neelakshi Das" }],
   robots: "index, follow",
   openGraph: {
-    title: "Crafting Stories",
-    description: "Neelakshi Das's Portfolio",
+    title: "Pixel Stories",
+    description: "Neelakshi's Portfolio",
     type: "website",
     images: [
       {
         url: "/og-image.png",
         width: 1200,
         height: 630,
-        alt: "Neelakshi Das Portfolio Preview",
+        alt: "Neelakshi's Portfolio Preview",
       },
     ],
   },
