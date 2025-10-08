d8fd9c5f43be29264ac2bbe9169d6cf42a02727e
bluecoder2003 <130232862+bluecoder2003@users.noreply.github.com>
2025-10-08 10:28:02 +0530

    feat: :art: updated the about section

--- components/sections/PersonSections.tsx
+++ components/sections/PersonSections.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import { Instrument_Serif } from "next/font/google";
+import { ArrowUpRight } from "lucide-react";
 
 const instrumentSerif = Instrument_Serif({
   subsets: ["latin"],
@@ -42,73 +43,166 @@ export default function PersonSections() {
   return (
     <section className="bg-white w-full max-w-7xl mx-auto h-fit p-4 md:p-[40px] rounded-[16px] flex flex-col gap-8 ">
       {/* Header */}
-      <div className="text-[28px] md:text-[32px] font-normal text-black mb-2">Behind The Pixels</div>
+      <div className="text-[28px] md:text-[32px] font-normal text-black mb-2">
+        Behind The Pixels
+      </div>
       <div className="flex flex-col justify-between items-center w-full max-w-5xl mx-auto">
-      {/* Top Row */}
-      <div className="flex flex-col justify-between items-center w-full">
-      <div className={`text-[#093FB4] text-2xl font-normal ${instrumentSerif.className} mb-4`}>Peek into my life</div>
-      <div className="flex flex-col md:flex-row justify-between items-center gap-8">
-        {/* Polaroid Grid */}
-        <Image src="/about-one.webp" alt="polaroid" width={1000} height={1000} className="object-contain w-full h-full md:w-[300px] md:h-fit lg:w-[400px] lg:h-fit" />
-        {/* Bio Text */}
-          <div className="text-black text-lg font-normal">
-            Born in Kolkata, the City of Joy — where every lane hums with art, culture, and color — I grew up surrounded by stories and creativity. That early chaos and charm shaped how I see and express.
-            <br /><br />
-            I speak Hindi, English, and Bengali — each with its own rhythm — and I&apos;m slowly picking up Spanish too. Wherever I go, that energy stays — a quiet anchor, a loud spark.
-        </div>
-        </div>
-      </div>
-      {/* Dashed Divider */}
-      <div className="w-full h-[1px] my-6 bg-[repeating-linear-gradient(to_right,_#BFBFAF_0_12px,_transparent_12px_24px)]" />
-
-      {/* Middle Row */}
-      {/* <div className={`text-[#093FB4] text-2xl block md:hidden font-normal ${instrumentSerif.className} mb-4`}>Places I&apos;ve worked before</div> */}
-        <div className="flex flex-col md:flex-row justify-between items-center gap-8">
-        <Image src="/about-two.webp" alt="polaroid" width={1000} height={1000} className="object-contain w-full h-full block md:hidden" />
-          <div className="text-black text-lg font-normal">
-            Creativity has always been my constant — from handmade cards to intuitive UI. I fell for design through its visual charm, but it&apos;s the research and problem-solving that keep me grounded.<br />
-            I&apos;m a curious soul with a love for mountains, travel, and the occasional deep-dive into random rabbit holes. Design lets me blend craft with clarity — and I wouldn&apos;t have it any other way.
-          </div>
-        <Image src="/about-two.webp" alt="polaroid" width={1000} height={1000} className="object-contain w-full h-full md:w-[300px] md:h-fit lg:w-[400px] lg:h-fit hidden md:block" />
-      </div>
-      {/* Timeline Header */}
-      <div className={`text-[#093FB4] text-2xl hidden md:block font-normal ${instrumentSerif.className} my-4`}>Places I&apos;ve worked before</div>
-      <div className={`text-[#093FB4] text-2xl block md:hidden font-normal ${instrumentSerif.className} my-8`}>Places I&apos;ve worked before</div> 
-      {/* Timeline */}
-      <div className="relative flex flex-col items-center w-full">
-        {/* Dashed Line */}
-        <div className="absolute w-full h-[1px] top-4 bg-[repeating-linear-gradient(to_right,_#BFBFAF_0_12px,_transparent_12px_24px)] z-0" />
-        
-        {/* Timeline Circles */}
-        {timeline.map((_, i) => (
+        {/* Top Row */}
+        <div className="flex flex-col justify-between items-center w-full">
           <div
-            key={i}
-            className={`absolute top-2.5 md:top-1.5 z-20 ${i === 0 ? 'md:left-[calc(13%-12px)] left-[calc(15%-1px)]' : ''} ${i === 1 ? 'md:left-[calc(40%-12px)] left-[calc(45%-12px)]' : ''} ${i === 2 ? 'md:left-[calc(68%-12px)] left-[calc(75%-24px)]' : ''} ${i === 3 ? 'md:left-[calc(95%-12px)] left-[calc(95%-1px)]' : ''}`}
+            className={`text-[#093FB4] text-2xl font-normal ${instrumentSerif.className} mb-4`}
           >
-            <div
-              className={`w-2 h-2 md:w-4 md:h-4 rounded-full border-[1px] flex items-center justify-center
-                ${i === timeline.length - 1 ? 'bg-[#BFBFAF] border-[#BFBFAF]' : 'bg-white border-[#BFBFAF]'}`}
-            >
-              {/* Optionally, inner dot for active */}
-              {i === timeline.length - 1 && <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-[#BFBFAF]" />}
+            Peek into my life
+          </div>
+          <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+            {/* Polaroid Grid */}
+            <Image
+              src="/about-one.webp"
+              alt="polaroid"
+              width={1000}
+              height={1000}
+              className="object-contain w-full h-full md:w-[300px] md:h-fit lg:w-[400px] lg:h-fit"
+            />
+            {/* Bio Text */}
+            <div className="text-black text-lg font-normal">
+              Born in Kolkata, the City of Joy — where every lane hums with art,
+              culture, and color — I grew up surrounded by stories and
+              creativity. That early chaos and charm shaped how I see and
+              express.
+              <br />
+              <br />I speak Hindi, English, and Bengali — each with its own
+              rhythm — and I&apos;m slowly picking up Spanish too. Wherever I
+              go, that energy stays — a quiet anchor, a loud spark.
             </div>
           </div>
-        ))}
+        </div>
 
-        {/* Timeline Items */}
-        <div className="flex flex-row justify-between w-full gap-4 md:gap-14 lg:gap-24 z-10 pt-4">
-          {timeline.map((item, i) => (
-            <div key={i} className="flex flex-col items-center w-1/4">
-              <div className="flex flex-col items-center mb-2">
-                <Image src={item.logo} alt="logo" width={100} height={100} className="object-contain w-full h-full mb-2" />
+        <div className="w-full h-[1px] my-6 bg-[repeating-linear-gradient(to_right,_#BFBFAF_0_12px,_transparent_12px_24px)]" />
+
+        <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+          <Image
+            src="/about-two.webp"
+            alt="polaroid"
+            width={1000}
+            height={1000}
+            className="object-contain w-full h-full block md:hidden"
+          />
+          <div className="text-black text-lg font-normal">
+            Creativity has always been my constant — from handmade cards to
+            intuitive UI. I fell for design through its visual charm, but
+            it&apos;s the research and problem-solving that keep me grounded.
+            <br />
+            I&apos;m a curious soul with a love for mountains, travel, and the
+            occasional deep-dive into random rabbit holes. Design lets me blend
+            craft with clarity — and I wouldn&apos;t have it any other way.
+          </div>
+          <Image
+            src="/about-two.webp"
+            alt="polaroid"
+            width={1000}
+            height={1000}
+            className="object-contain w-full h-full md:w-[300px] md:h-fit lg:w-[400px] lg:h-fit hidden md:block"
+          />
+        </div>
+        <div className="w-full h-[1px] my-6 bg-[repeating-linear-gradient(to_right,_#BFBFAF_0_12px,_transparent_12px_24px)]" />
+        <div className="flex flex-col md:flex-row justify-between items-center lg:gap-24 gap-12">
+          {/* Polaroid Grid */}
+          <Image
+            src="/figma.png"
+            alt="polaroid"
+            width={1000}
+            height={1000}
+            className="object-contain w-full h-full md:w-[300px] md:h-fit lg:w-[600px] lg:h-fit lg:pl-12 pl-0"
+          />
+          {/* Bio Text */}
+          <div className="flex flex-col gap-2">
+            <div className="text-black text-lg font-normal">
+              Also...One of my most exciting moments was being featured at the
+              Figma Makeathon, recognized among 10,000+ participants.
+              <br />
+              Seeing a project I built from scratch stand out on a global stage
+              was surreal — both humbling and electrifying — and reminded me why
+              I love creating
+            </div>
+            <button onClick={() => window.open("https://x.com/figma/status/1968007805203517458", "_blank")} className="bg-black text-white text-sm font-normal px-3 py-1 rounded-full shadow-md w-fit flex whitespace-nowrap justify-center items-center gap-1">
+              Link
+              <ArrowUpRight className="w-4 h-4 text-white" />
+            </button>
+          </div>
+        </div>
+        <div className="w-full h-[1px] my-6 bg-[repeating-linear-gradient(to_right,_#BFBFAF_0_12px,_transparent_12px_24px)]" />
+        {/* Timeline Header */}
+        <div
+          className={`text-[#093FB4] text-2xl hidden md:block font-normal ${instrumentSerif.className} my-4`}
+        >
+          Places I&apos;ve worked before
+        </div>
+        <div
+          className={`text-[#093FB4] text-2xl block md:hidden font-normal ${instrumentSerif.className} my-8`}
+        >
+          Places I&apos;ve worked before
+        </div>
+        {/* Timeline */}
+        <div className="relative flex flex-col items-center w-full">
+          {/* Dashed Line */}
+          <div className="absolute w-full h-[1px] top-4 bg-[repeating-linear-gradient(to_right,_#BFBFAF_0_12px,_transparent_12px_24px)] z-0" />
+
+          {/* Timeline Circles */}
+          {timeline.map((_, i) => (
+            <div
+              key={i}
+              className={`absolute top-2.5 md:top-1.5 z-20 ${
+                i === 0 ? "md:left-[calc(13%-12px)] left-[calc(15%-1px)]" : ""
+              } ${
+                i === 1 ? "md:left-[calc(40%-12px)] left-[calc(45%-12px)]" : ""
+              } ${
+                i === 2 ? "md:left-[calc(68%-12px)] left-[calc(75%-24px)]" : ""
+              } ${
+                i === 3 ? "md:left-[calc(95%-12px)] left-[calc(95%-1px)]" : ""
+              }`}
+            >
+              <div
+                className={`w-2 h-2 md:w-4 md:h-4 rounded-full border-[1px] flex items-center justify-center
+                ${
+                  i === timeline.length - 1
+                    ? "bg-[#BFBFAF] border-[#BFBFAF]"
+                    : "bg-white border-[#BFBFAF]"
+                }`}
+              >
+                {/* Optionally, inner dot for active */}
+                {i === timeline.length - 1 && (
+                  <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-[#BFBFAF]" />
+                )}
               </div>
-              <div className="text-black text-[15px] font-normal text-center leading-tight">{item.role}</div>
-              <div className="text-[#093FB4] text-[14px] font-normal text-center leading-tight mt-1 hidden md:block">{item.date}</div>
-              <div className="text-zinc-500 text-[13px] font-normal text-center leading-tight hidden md:block">{item.duration}</div>
             </div>
           ))}
+
+          {/* Timeline Items */}
+          <div className="flex flex-row justify-between w-full gap-4 md:gap-14 lg:gap-24 z-10 pt-4">
+            {timeline.map((item, i) => (
+              <div key={i} className="flex flex-col items-center w-1/4">
+                <div className="flex flex-col items-center mb-2">
+                  <Image
+                    src={item.logo}
+                    alt="logo"
+                    width={100}
+                    height={100}
+                    className="object-contain w-full h-full mb-2"
+                  />
+                </div>
+                <div className="text-black text-[15px] font-normal text-center leading-tight">
+                  {item.role}
+                </div>
+                <div className="text-[#093FB4] text-[14px] font-normal text-center leading-tight mt-1 hidden md:block">
+                  {item.date}
+                </div>
+                <div className="text-zinc-500 text-[13px] font-normal text-center leading-tight hidden md:block">
+                  {item.duration}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
       </div>
     </section>
   );
